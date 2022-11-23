f70565fdc4d19060e4f94ca98e826741b995ea17
SFXDevel <87386985+ShoneGK@users.noreply.github.com>
2022-11-23 16:34:33 -0500

    properly placed hotbar + proper fiiltering

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
  A simple version of minecraft in a 2d space
 
 ## TODO
-- [ ] Fix intellisense to recognize raylib.h
+- [x] Fix intellisense to recognize raylib.h
+- [ ] Add the ability to load a real minecraft texture pack
 - [ ] Add basic fixed flat world with animated character
 - [ ] add procedural generation
--- Src/World.cpp
+++ Src/World.cpp
@@ -1,16 +1,30 @@
 #include "raylib.h"
+#include "raymath.h"
+
+const int screenWidth = 1500;
+const int screenHeight = 600;
+
+int hotBarScale = 2;
 
 int main()
 {
-    InitWindow(800, 600, "Minecraft2D");
+    InitWindow(screenWidth, screenHeight, "Minecraft2D");
+
+    // setting up the hotbar
+    Texture2D hotBar = LoadTexture("Assets/Textures/MC2D/Assets/GUI/hotbar.png");
+    Rectangle hotBarSrc = {0,0,182,22};
+    Rectangle hotBarDest = {screenWidth/2 - hotBar.width/2 * hotBarScale, screenHeight - 2 - hotBar.height/2 - 30 * hotBarScale, hotBar.width * hotBarScale, hotBar.height * hotBarScale};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
         ClearBackground(BLUE);
+        
+        DrawTexturePro(hotBar, hotBarSrc, hotBarDest, Vector2Zero(), 0, WHITE);
 
         DrawFPS(10, 10);
+     
         EndDrawing();
     }
     CloseWindow();
