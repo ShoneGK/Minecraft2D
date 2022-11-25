ad44bacdff412a402c8e3f971404e29f14e037ad
SFXDevel <87386985+ShoneGK@users.noreply.github.com>
2022-11-25 11:15:13 -0500

    properly recalculated hotbar location

--- Src/World.cpp
+++ Src/World.cpp
@@ -1,22 +1,38 @@
 #include "raylib.h"
 #include "raymath.h"
 
-float screenWidth = 1500;
-float screenHeight = 600;
+// debugging
+#include <iostream>
+
+int screenWidth = 600;
+int screenHeight = 300;
 
 int hotBarScale = 2;
 
 int main()
 {
+    // allow the window to be resized by the user
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
     InitWindow(screenWidth, screenHeight, "Minecraft2D");
 
     // setting up the hotbar
     Texture2D hotBar = LoadTexture("Assets/Textures/MC2D/Assets/GUI/hotbar.png");
     Rectangle hotBarSrc = {0,0,182,22};
-    Rectangle hotBarDest = {screenWidth/2 - hotBar.width/2 * hotBarScale, screenHeight - 2 - hotBar.height/2 - 30 * hotBarScale, hotBar.width * hotBarScale, hotBar.height * hotBarScale};
+    Rectangle hotBarDest = {screenWidth/2 - hotBar.width/2 * hotBarScale, screenHeight - hotBar.height/2 - 20 * hotBarScale, hotBar.width * hotBarScale, hotBar.height * hotBarScale};
 
     while (!WindowShouldClose())
     {
+        // if the current screen dimensions are diffrent
+        // from the old screen dimensions, recalculate all GUI components
+        if(IsWindowResized()){
+            screenHeight = GetScreenHeight();
+            screenWidth = GetScreenWidth();
+            hotBarDest.x = screenWidth/2 - hotBar.width/2 * hotBarScale;
+            hotBarDest.y = screenHeight - hotBar.height/2 - 20 * hotBarScale;
+            std::cout << "Display Resized : " << "(" << screenWidth << "," << screenHeight << ")" << std::endl;
+        }
+        
         BeginDrawing();
 
         ClearBackground(BLUE);
